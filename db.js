da446cc8184e02f668e20ba557f60c711cdf5294
bhumika03-dev <ladibhumika03@gmail.com>
2025-05-22 14:25:41 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 3306,
+  host: process.env.DB_HOST,       // from Railway env vars
+  user: process.env.DB_USER,       // from Railway env vars
+  password: process.env.DB_PASSWORD, // from Railway env vars
+  database: process.env.DB_NAME,   // from Railway env vars
+  port: 3306                      // default MySQL port (usually Railway uses this)
 });
 
 module.exports = pool.promise();
